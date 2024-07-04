e253d80aaadac226090e61011dff560f0960fe0a
Reihan Faizal Hanif <mk.reihan.hanif@pertamina.com>
2024-07-04 11:11:44 +0700

    pertemuan 5

--- Pertemuan6_SinauCoding/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import LeftSide from "./components/LeftSide.vue";
-import RightSide from "./components/RightSide.vue";
-</script>
-
-<template>
-  <LeftSide/>
-  <RightSide/>
-</template>
